a69e48fdc85765f15a3038bc64c41a78124d7182
02.000500 <02.000500@ELOM00XF8-N.elo.elopar.corp>
2019-10-02 13:09:25 -0300

    Testei o sysout do getTotal criado na class Conta

--- bytebank-encapsulado/src/TestaValores.java
+++ bytebank-encapsulado/src/TestaValores.java
@@ -11,6 +11,13 @@ public class TestaValores {
 		
 		System.out.println(conta.getAgencia());
 		
+		conta.setAgencia(1232123);
+		
+		Conta conta2 = new Conta(1337, 16549);
+		Conta conta3 = new Conta(2112, 14660);
+		
+		System.out.println(Conta.getTotal());
+		
 	}
 
 }
